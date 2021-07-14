810e5bda3bb6043339c2858f05355c7749175af6
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2021-07-14 11:19:05 +0900

    resnet 수정
    resnet 수정

--- resnet.py
+++ resnet.py
@@ -29,10 +29,10 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.silu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = F.silu(out)
         return out
 
 
@@ -56,11 +56,11 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        out = F.silu(self.bn1(self.conv1(x)))
+        out = F.silu(self.bn2(self.conv2(out)))
         out = self.bn3(self.conv3(out))
         out += self.shortcut(x)
-        out = F.relu(out)
+        out = F.silu(out)
         return out
 
 
@@ -72,6 +72,7 @@ class ResNet(nn.Module):
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.dropout=nn.dropout(0.3)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
@@ -86,11 +87,11 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        out = F.Silu(self.bn1(self.conv1(x)))
+        out = self.dropout(self.layer1(out))
+        out = self.dropout(self.layer2(out))
+        out = self.dropout(self.layer3(out))
+        out = self.dropout(self.layer4(out))
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
@@ -102,7 +103,7 @@ def ResNet18():
     #return ResNet(BasicBlock, [2,2,2,2])
 
 def ResNet34():
-    return ResNet(BasicBlock, [3,4,6,6])    #ì›ë³¸ : [3,4,6,3]
+    return ResNet(BasicBlock, [3,4,6,41])    #?›ë³? : [3,4,6,3]
 
 def ResNet50():
     return ResNet(Bottleneck, [3,4,6,3])
@@ -115,8 +116,8 @@ def ResNet152():
 
 
 def test():
-    net = ResNet34()       #18ì—ì„œ 34ë¡œ ë³€ê²½
-    y = net(torch.randn(1,3,32,32)) #ì›ë³¸ : (1,3,32,32)
+    net = ResNet34()       #18?—?„œ 34ë¡? ë³?ê²?
+    y = net(torch.randn(1,3,32,32)) #?›ë³? : (1,3,32,32)
     print(y.size())
 
 # test()
